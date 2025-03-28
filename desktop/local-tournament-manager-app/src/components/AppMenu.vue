08f8c9197ba4305b789826979290f78fb577e27a
Federico <brogi.federico@gmail.com>
2025-03-28 23:25:50 +0100

    added ServerStatus.vue

--- desktop/local-tournament-manager-app/src/components/AppMenu.vue
+++ desktop/local-tournament-manager-app/src/components/AppMenu.vue
@@ -1,12 +1,10 @@
 <template>
   <nav class="bg-emerald-800 text-white px-4 py-3 shadow-md">
     <div class="flex items-center justify-between">
-      <div class="text-xl font-semibold">LocalTournamentManager</div>
-      <ul class="flex space-x-4 text-sm">
-        <li><router-link to="/" class="hover:underline">Home</router-link></li>
-        <li><router-link to="/submit" class="hover:underline">Invia Risultato</router-link></li>
-        <li><router-link to="/admin" class="hover:underline">Gestione</router-link></li>
-      </ul>
+      <div class="text-xl font-semibold"><router-link to="/" class="hover:underline">LocalTournamentManager</router-link></div>
+      <div class="flex space-x-4 text-sm">
+        <ServerStatus/>
+      </div>
     </div>
   </nav>
 </template>
@@ -17,3 +15,6 @@
   text-decoration: underline;
 }
 </style>
+<script setup lang="ts">
+import ServerStatus from "./ServerStatus.vue";
+</script>
